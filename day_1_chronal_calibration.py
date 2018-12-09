9fe9b5e6787c533552912c92b5516bfcf18d7ae2
Jan Chwiejczak <jchwiejczak@gmail.com>
2018-12-09 22:48:59 +0000

    Add second part day1

--- day_1_chronal_calibration.py
+++ day_1_chronal_calibration.py
@@ -1,8 +1,23 @@
-STARTING_FREQUENCY = 0
+def frequencies_from_input(input_fname):
+    with open(input_fname, 'rt') as f:
+        return list(map(eval, f))
 
-lines = open('day_1_input.txt', 'rt').readlines()
-lines_stripped = (line.strip() for line in lines)
-frequencies = map(int, lines_stripped)
-resulting_frequency = sum(frequencies)
 
-print(f'Resulting frequency is: {resulting_frequency}')
+def first_repeating_frequency(frequencies, current_frequency=0):
+    frequencies_seen = set()
+    frequencies_seen.add(current_frequency)
+    while True:
+        for f in frequencies:
+            current_frequency += f
+            if current_frequency in frequencies_seen:
+                return current_frequency
+            frequencies_seen.add(current_frequency)
+
+
+if __name__ == '__main__':
+    frequencies = frequencies_from_input('day_1_input.txt')
+    first_frequency = sum(frequencies)
+    print(f'First frequency is: {first_frequency}')
+
+    repeating_frequency = first_repeating_frequency(frequencies)
+    print(f'First repeating frequency is: {repeating_frequency}')
