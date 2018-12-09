c999e271dd5422c79750dfb485a631bc36c1c0c5
Jan Chwiejczak <jchwiejczak@gmail.com>
2018-12-09 23:13:18 +0000

    Refactor brake down finding duplicate and generating sequence of frequencies

--- day_1_chronal_calibration.py
+++ day_1_chronal_calibration.py
@@ -1,23 +1,47 @@
 import itertools
+import pytest
 
 
-def frequencies_from_input(input_fname):
+def deltas_from_input(input_fname):
     with open(input_fname, 'rt') as f:
         return list(map(eval, f))
 
 
-def first_repeating_frequency(frequencies, current_frequency=0):
-    frequencies_seen = set()
-    frequencies_seen.add(current_frequency)
-    for f in itertools.cycle(frequencies):
-        current_frequency += f
-        if current_frequency in frequencies_seen:
-            return current_frequency
-        frequencies_seen.add(current_frequency)
+def frequencies(changes):
+    f = 0
+    yield f
+    for ch in changes:
+        f += ch
+        yield f
+
+
+def first_duplicate(sequence):
+    seen = set()
+    for value in sequence:
+        if value in seen:
+            return value
+        seen.add(value)
 
 
 if __name__ == '__main__':
-    frequencies = frequencies_from_input('day_1_input.txt')
+    deltas = deltas_from_input('day_1_input.txt')
+    print(f'First frequency is: {sum(deltas)}')
+    print(f'First repeating frequency is: {first_duplicate(frequencies(itertools.cycle(deltas)))}')
 
-    print(f'First frequency is: {sum(frequencies)}')
-    print(f'First repeating frequency is: {first_repeating_frequency(frequencies)}')
+
+# Tests
+@pytest.mark.parametrize('sequence, duplicate', [
+    ('masfdmfdsfa', 'm'),
+    ([1, 2, 3, 4, 1], 1),
+    ([0, 0], 0),
+])
+def test_first_duplicate(sequence, duplicate):
+    assert first_duplicate(sequence) == duplicate
+
+@pytest.mark.parametrize('deltas, partial_sums', [
+    ([1, 2, 3, 4], [0, 1, 3, 6, 10]),
+    ([0, 0], [0, 0, 0]),
+    ([-1, 2, 1], [0, -1, 1, 2]),
+])
+def test_frequencies(deltas, partial_sums):
+    assert list(frequencies(deltas)) == partial_sums
