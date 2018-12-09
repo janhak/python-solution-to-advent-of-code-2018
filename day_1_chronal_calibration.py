d36f6fda464bfaabfc13142785cb77ce98bbc250
Jan Chwiejczak <jchwiejczak@gmail.com>
2018-12-09 22:52:02 +0000

    Inline answer vars remove while loop

--- day_1_chronal_calibration.py
+++ day_1_chronal_calibration.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 def frequencies_from_input(input_fname):
     with open(input_fname, 'rt') as f:
         return list(map(eval, f))
@@ -6,18 +9,15 @@ def frequencies_from_input(input_fname):
 def first_repeating_frequency(frequencies, current_frequency=0):
     frequencies_seen = set()
     frequencies_seen.add(current_frequency)
-    while True:
-        for f in frequencies:
-            current_frequency += f
-            if current_frequency in frequencies_seen:
-                return current_frequency
-            frequencies_seen.add(current_frequency)
+    for f in itertools.cycle(frequencies):
+        current_frequency += f
+        if current_frequency in frequencies_seen:
+            return current_frequency
+        frequencies_seen.add(current_frequency)
 
 
 if __name__ == '__main__':
     frequencies = frequencies_from_input('day_1_input.txt')
-    first_frequency = sum(frequencies)
-    print(f'First frequency is: {first_frequency}')
 
-    repeating_frequency = first_repeating_frequency(frequencies)
-    print(f'First repeating frequency is: {repeating_frequency}')
+    print(f'First frequency is: {sum(frequencies)}')
+    print(f'First repeating frequency is: {first_repeating_frequency(frequencies)}')
