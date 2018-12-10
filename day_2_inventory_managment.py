509ab6ffbaac90e23f416a3444e44867a17b3168
Jan Chwiejczak <jchwiejczak@gmail.com>
2018-12-10 20:32:52 +0000

    Add O(n) solution to part 2

--- day_2_inventory_managment.py
+++ day_2_inventory_managment.py
@@ -1,5 +1,5 @@
 # https://adventofcode.com/2018/day/2
-from collections import Counter
+from collections import Counter, defaultdict
 import pytest
 import operator
 import itertools
@@ -10,6 +10,7 @@ def lines(input_fname):
         return f.read().splitlines()
 
 
+# First part
 def count_letters(box_id):
     letter_twice = 0
     letter_thrice = 0
@@ -22,6 +23,7 @@ def count_letters(box_id):
     return (letter_twice, letter_thrice)
 
 
+# Brute Force
 def one_char_diff(first, second):
     """Are first and second equal except for one character"""
     differences = sum((x != y) for x, y in zip(first, second))
@@ -39,13 +41,35 @@ def common_characters(first, second):
     return ''.join(x for x, y in zip(first, second) if x == y)
 
 
+# Quick solution
+def blank_one_char(a_string):
+    """Yield all strings with one char blanked"""
+    for i in range(len(a_string)):
+        yield a_string[:i] + '_' + a_string[i+1:]
+
+
+def find_two_similar_fast(box_ids):
+    """Find first pair of box_ids that differ by one character"""
+    blanked = defaultdict(list)
+    for box in box_ids:
+        for blank in blank_one_char(box):
+            blanked[blank].append(box)
+
+    return next(v for v in blanked.values() if len(v) == 2, None)
+
+
 if __name__ == '__main__':
     box_ids = list(lines('day_2_input.txt'))
     counted_ids = (count_letters(box_id) for box_id in box_ids)
     count = (sum(x) for x in zip(*counted_ids))
     print(f'Checksum for box ids is: {operator.mul(*count)}')
 
+    # Brute Force
     boxes = find_two_similar_boxes(box_ids)
+    print(f'Letters that are the same between two similar boxes are: {common_characters(*boxes)}')
+
+    # Quick
+    boxes = find_two_similar_fast(box_ids)
     print(f'Letters that are the same between two similar boxes are: {common_characters(*boxes)}')
 
 
