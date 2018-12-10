94cc32f5ef062c0412f437f9b11e9f2ccb20b3f9
Jan Chwiejczak <jchwiejczak@gmail.com>
2018-12-10 19:51:56 +0000

    Second part BF

--- day_2_inventory_managment.py
+++ day_2_inventory_managment.py
@@ -2,6 +2,8 @@
 from collections import Counter
 import pytest
 import operator
+import itertools
+
 
 def lines(input_fname):
     with open(input_fname, 'rt') as f:
@@ -21,11 +23,37 @@ def count_letters(box_id):
     return (letter_twice, letter_thrice)
 
 
+def one_char_diff(first, second):
+    differences = 0
+    for x, y in zip(first, second):
+        if x != y:
+            differences += 1
+        if differences > 1:
+            return False
+    return differences == 1
+
+
+def find_two_similar_boxes(box_ids):
+    for x, y in itertools.product(box_ids, box_ids):
+        if one_char_diff(x, y):
+            return x, y
+
+
+def filter_out_diff(first, second):
+    for x, y in zip(first, second):
+        if x == y:
+            yield x
+
+
 if __name__ == '__main__':
-    box_ids = lines('day_2_input.txt')
+    box_ids = list(lines('day_2_input.txt'))
     counted_ids = (count_letters(box_id) for box_id in box_ids)
     count = (sum(x) for x in zip(*counted_ids))
     print(f'Checksum for box ids is: {operator.mul(*count)}')
+
+    boxes = find_two_similar_boxes(box_ids)
+    same = ''.join(filter_out_diff(*boxes))
+    print(f'Letters that are the same between two similar boxes are: {same}')
 
 
 # Tests
@@ -40,3 +68,11 @@ if __name__ == '__main__':
 ])
 def test_count_letters(an_id, count):
     assert count_letters(an_id) == count
+
+@pytest.mark.parametrize('first, second, diff', [
+    ('aabc', 'aacc', True),
+    ('aaaa', 'aaaa', False),
+    ('abbc', 'aaaa', False),
+])
+def test_one_char_diff(first, second, diff):
+    assert one_char_diff(first, second) == diff
